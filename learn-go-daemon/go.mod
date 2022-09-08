ab2773e01753116599eecde288a7d78438c69cf3
tanxiaohui <xiaohui.tan@greatdb.com>
2022-09-08 17:23:25 +0800

    学习go daemon

---
+++ learn-go-daemon/go.mod
@@ -0,0 +1,10 @@
+module aaa
+
+go 1.18
+
+require github.com/sevlyar/go-daemon v0.1.6
+
+require (
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	golang.org/x/sys v0.0.0-20220907062415-87db552b00fd // indirect
+)
---
+++ learn-go-daemon/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sevlyar/go-daemon"
+)
+
+// 要终止守护进程，使用:
+// kill `cat sample.pid`。
+func main() {
+
+	context := &daemon.Context{
+		PidFileName: "sample.pid",
+		PidFilePerm: 0644,
+		LogFileName: "sample.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[go-daemon sample]"},
+	}
+
+	d, err := context.Reborn() //在父进程返回*os.Process 在子进程程返回的是nil 其他情况返回错误
+	if err != nil {
+		log.Fatal("创建守护进程失败, err:", err.Error())
+	}
+	if d != nil {
+		log.Printf("这是父进程的标志")
+		return
+	}
+
+	defer func(context *daemon.Context) {
+		err := context.Release()
+		if err != nil {
+			log.Printf("释放失败:%s", err.Error())
+		}
+	}(context)
+
+	log.Print("- - - - - - - - - - - - - - -")
+	log.Print("守护进程启动!!!!!")
+
+	serveHTTP()
+}
+
+func serveHTTP() {
+	log.Printf("参数:%v",os.Args)
+	http.HandleFunc("/", httpHandler)
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	if err != nil {
+		fmt.Printf("启动服务失败, err:%s",err.Error())
+		return
+	}
+}
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("request from %s: %s %q", r.RemoteAddr, r.Method, r.URL)
+	_, err := fmt.Fprintf(w, "go-daemon: %q", html.EscapeString(r.URL.Path))
+	if err != nil {
+		log.Printf("返回信息失败, err:%s",err)
+		return
+	}
+}
