1289f07897fed48d983d9ce537ebb183207fa674
tanxiaohui <xiaohui.tan@greatdb.com>
2022-09-09 10:00:18 +0800

    go-dameon

--- learn-go-daemon/main.go
+++ learn-go-daemon/main.go
@@ -19,9 +19,9 @@ func main() {
 		PidFilePerm: 0644,
 		LogFileName: "sample.log",
 		LogFilePerm: 0640,
-		WorkDir:     "./",
-		Umask:       027,
-		Args:        []string{"[go-daemon sample]"},
+		WorkDir:     "./",                        //创建子进程时会切到该目录
+		Umask:       027,                         //If Umask is non-zero, the daemon-process call Umask() func with given value.
+		Args:        []string{"go-daemon守护进程服务"}, //传递给子进程的参数 是nil就用os.Args
 	}
 
 	d, err := context.Reborn() //在父进程返回*os.Process 在子进程程返回的是nil 其他情况返回错误
@@ -38,20 +38,20 @@ func main() {
 		if err != nil {
 			log.Printf("释放失败:%s", err.Error())
 		}
+		log.Printf("释放成功!!!")
 	}(context)
 
-	log.Print("- - - - - - - - - - - - - - -")
 	log.Print("守护进程启动!!!!!")
-
 	serveHTTP()
+	log.Print("服务退出了~~~")
 }
 
 func serveHTTP() {
-	log.Printf("参数:%v",os.Args)
+	log.Printf("参数:%v", os.Args)
 	http.HandleFunc("/", httpHandler)
 	err := http.ListenAndServe("127.0.0.1:8080", nil)
 	if err != nil {
-		fmt.Printf("启动服务失败, err:%s",err.Error())
+		fmt.Printf("启动服务失败, err:%s", err.Error())
 		return
 	}
 }
@@ -60,7 +60,7 @@ func httpHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("request from %s: %s %q", r.RemoteAddr, r.Method, r.URL)
 	_, err := fmt.Fprintf(w, "hello 你好啊!: %q", html.EscapeString(r.URL.Path))
 	if err != nil {
-		log.Printf("返回信息失败, err:%s",err)
+		log.Printf("返回信息失败, err:%s", err)
 		return
 	}
 }
